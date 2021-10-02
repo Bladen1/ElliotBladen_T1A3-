be02c1b776de8ab591db203c1307da2848e9816c
Elliot Bladen <e_bladen@hotmail.com>
2021-10-02 11:18:41 +1000

    long form added

---
+++ long_form.rb
@@ -0,0 +1,60 @@
+f = File.read('horse_form.txt')
+e = File.read('horse_form.txt')
+winx = File.readlines('long_form.txt')[0]
+maykbediva = File.readlines('long_form.txt')[1]
+sunline = File.readlines('long_form.txt')[2]
+northerly = File.readlines('long_form.txt')[3]
+octaganol = File.readlines('long_form.txt')[4]
+lonhro = File.readlines('long_form.txt')[5]
+soyouthink = File.readlines('long_form.txt')[6]
+saintly = File.readlines('long_form.txt')[7]
+repeat = "Please enter number between 1-9"
+a = 9
+number = 0
+c = 9
+m = 5
+puts f
+while true
+   
+    puts "Please press horse number (1-8) to see form. Or press (9) to go back"
+    number = gets.chomp.to_f
+    if number == 1 then
+        puts winx
+    end
+    if number == 2 then
+        puts maykbediva
+    end
+    if number == 3 then
+        puts sunline
+    end
+    if number == 4 then
+        puts northerly
+    end
+     if number == 5 then
+        puts octaganol
+     end
+    if number == 6 then
+        puts lonhro
+    end
+    if number == 7 then
+         puts soyouthink
+    end
+    if number == 8 then
+          puts saintly
+    end
+      if number == a
+            puts e
+       if
+            number = "m".to_s
+
+            require_relative './betslip'
+        end
+                
+            end
+
+        end
+    
+      
+
+
+
